2d544f8e4c7d9bc33b36fb213aa613ebe67661d3
hibiskush <endercraft04vn@gmail.com>
2025-01-06 08:07:10 +0000

    update main.dart

--- GKY/giuaki_22010263/lib/main.dart
+++ GKY/giuaki_22010263/lib/main.dart
@@ -1,31 +1,20 @@
 import 'package:flutter/material.dart';
-
-class User {
-  String username;
-  String password;
-  String role;
-
-  User({required this.username, required this.password, required this.role});
-}
+// import 'user.dart';
+import 'user_data.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<User> users = [
-    User(username: 'user1', password: 'pass1', role: 'admin'),
-    User(username: 'user2', password: 'pass2', role: 'user'),
-    User(username: 'user3', password: 'pass3', role: 'guest'),
-    User(username: 'user4', password: 'pass4', role: 'admin'),
-    User(username: 'user5', password: 'pass5', role: 'user'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('User Grid')),
+        appBar: AppBar(
+          title: Text('Kiem tra Giua Ky'),
+          backgroundColor: Colors.blueAccent, 
+        ),
         body: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
@@ -33,11 +22,12 @@ class MyApp extends StatelessWidget {
           itemCount: users.length,
           itemBuilder: (context, index) {
             return Card(
+              color: Colors.blueAccent, 
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Username: ${users[index].username}'),
-                  Text('Role: ${users[index].role}'),
+                  Text('Username: ${users[index].username}', style: TextStyle(color: Colors.white)),
+                  Text('Role: ${users[index].role}', style: TextStyle(color: Colors.white)),
                 ],
               ),
             );
---
+++ GKY/giuaki_22010263/lib/user_data.dart
@@ -0,0 +1,9 @@
+import 'user.dart';
+
+final List<User> users = [
+  User(username: 'user1', password: 'pass1', role: 'admin'),
+  User(username: 'user2', password: 'pass2', role: 'user'),
+  User(username: 'user3', password: 'pass3', role: 'guest'),
+  User(username: 'user4', password: 'pass4', role: 'admin'),
+  User(username: 'user5', password: 'pass5', role: 'user'),
+];
