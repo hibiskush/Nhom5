510ed8cb99286926a35ac095dab2131846f9a1c8
14tow857 <121293269+14tow857@users.noreply.github.com>
2025-02-24 09:10:48 +0000

    Fixed coingecko headers, added option for pro-api

--- DevReviews/app/lib/services/coingecko_service.dart
+++ DevReviews/app/lib/services/coingecko_service.dart
@@ -1,13 +1,27 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class CoinGeckoService {
-  final String baseUrl = "https://api.coingecko.com/api/v3";
+  late final String apiKey;
+  late final String root;
+  late final String baseUrl;
+  late final headers;
+
+  CoinGeckoService() {
+    apiKey = dotenv.env['COINGECKO_API_KEY'] ?? '';
+    root = apiKey.isEmpty ? "https://api.coingecko.com" : "https://pro-api.coingecko.com";
+    baseUrl = "$root/api/v3";
+    headers = {
+      "X-Requested-With": "XMLHttpRequest",
+      "x-cg-pro-api-key": apiKey,
+    };
+  }
 
   Future<double?> getCryptoPrice(String cryptoId, String currency) async {
     final url = Uri.parse("$baseUrl/simple/price?ids=$cryptoId&vs_currencies=$currency");
 
-    final response = await http.get(url);
+    final response = await http.get(url, headers: headers);
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
@@ -20,7 +34,7 @@ class CoinGeckoService {
   Future<double?> getCryptoPriceChange(String cryptoId, String currency) async {
     final url = Uri.parse("$baseUrl/simple/price?ids=$cryptoId&vs_currencies=$currency&include_24hr_change=true");
 
-    final response = await http.get(url);
+    final response = await http.get(url, headers: headers);
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
